4671d42971278dc24e2ed5edbfa43d64ed04f6cb
Ogbevire O., Obogrow <ogbevire@gmail.com>
2023-11-29 10:13:49 +0100

    using post

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 //   // res.send({color : 'red'});
 //   res.send(`<h1>This is my webpage</h1>, { color: 'red' }`);
 // })
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
   res.send('This is the homepage')
 })
 app.get('/cats', (req, res) => {
@@ -15,6 +15,19 @@ app.get('/cats', (req, res) => {
 
 app.get('/dogs', (req, res) => {
   res.send('Woof!!!')
+})
+
+app.post('/cats', (req, res) => {
+  res.send('This is a post to the server!!!')
+})
+
+//catch all
+app.get('*', (req, res) => {
+  res.send(`This path doesn't exist.`)
+})
+
+app.post('*', (req, res) => {
+  res.send(`This path doesn't exist.`)
 })
 
 app.listen(3000, () => {
